d4da90cd718af7cd5088d30fd532cc72db821498
samrat <samtina.basu@gmail.com>
2021-09-02 20:34:10 +0530

    Updated Security policy

--- src/main/java/com/org/skillzag/assesment/config/SecurityConfiguration.java
+++ src/main/java/com/org/skillzag/assesment/config/SecurityConfiguration.java
@@ -50,6 +50,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            .anyRequest().authenticated()
         .and()
             .oauth2ResourceServer()
                 .jwt()
